9f6e41ee4f9ed35b220bc15534192ed25eb4c699
Dean99712 <Dean2910997@gmail.com>
2022-08-23 06:12:09 +0300

    added 3 variables to MainActivity.kt and created editText in activity_main.xml

--- app/src/main/java/com/example/hackeruapp/MainActivity.kt
+++ app/src/main/java/com/example/hackeruapp/MainActivity.kt
@@ -3,23 +3,36 @@ package com.example.hackeruapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import retrofit2.Retrofit
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button: Button = findViewById(R.id.hello_button)
+        var editText: EditText = findViewById(R.id.name)
+        var welcome: TextView = findViewById(R.id.hello_text)
+
+        button.setOnClickListener {
+            val name = editText.text.toString()
+
+            if (name.isEmpty())
+                Toast
+                    .makeText(
+                        this,
+                        "Please Enter you name!",
+                        Toast.LENGTH_SHORT
+                    )
+                    .show()
+            else
+                welcome.text = "Hello ${name} ";
+
+        }
+
     }
 
-    fun getName(): String {
-        var name:String = "Yakov"
-        name = " Nevo"
-        return name
-    }
-
-    fun onButtonClick(view:View) {
-        val textView:TextView = findViewById<TextView>(R.id.hello_text)
-       textView.text = "Hello ${getName()} How are you"
-    }
 }
