d423b48897b0cc601d9f2e34871ace58bfbbf00c
Dean99712 <Dean2910997@gmail.com>
2022-09-01 10:44:58 +0300

    fixed recyclerView method in MainActivity

--- app/src/main/java/com/example/hackeruapp/MainActivity.kt
+++ app/src/main/java/com/example/hackeruapp/MainActivity.kt
@@ -2,32 +2,34 @@ package com.example.hackeruapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.ListView
+import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: RecyclerAdapter
+    private lateinit var personList: ArrayList<Person>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setButton3ClickListener()
-
-        }
+    }
 
     private fun setButton3ClickListener() {
         val button = findViewById<Button>(R.id.add_button)
         button.setOnClickListener {
-            createList()
-//            createRecyclerView()
+            createRecyclerView()
 
         }
 
     }
 
-    private fun getPersonList(): MutableList<Person> {
+    private fun getPersonList(): ArrayList<Person> {
 
-        val personList = mutableListOf<Person>()
+        val personList = arrayListOf<Person>()
 
         personList.add(Person("Daniella", 30))
         personList.add(Person("Boaz", 52))
@@ -40,20 +42,14 @@ class MainActivity : AppCompatActivity() {
         return personList
     }
 
-    private fun createList() {
-        val listView = findViewById<ListView>(R.id.list_view)
-        val personList = getPersonList()
-        val myAdapter = ArrayAdapter(this, R.layout.item_layout, personList)
-        listView.adapter = myAdapter
-    }
 
-//    private fun createRecyclerView() {
-//        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-//        val personList = getPersonList()
-//        val adapter = RecyclerAdapter(personList)
-//        recyclerView.adapter = adapter
-//
-//
-//    }
+    private fun createRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        val personList = getPersonList()
+        val adapter = RecyclerAdapter(personList)
+        recyclerView.adapter = adapter
+
+
+    }
 
 }
--- app/src/main/java/com/example/hackeruapp/RecyclerAdapter.kt
+++ app/src/main/java/com/example/hackeruapp/RecyclerAdapter.kt
@@ -3,30 +3,29 @@ package com.example.hackeruapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerAdapter(private var dataList:List<Person>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+class RecyclerAdapter(private val personList: ArrayList<Person>)
+    : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView
-        init {
-            textView = view.findViewById(R.id.row_text)
-//            textView.text = "Ron"
-        }
+        val textView: TextView = itemView.findViewById(R.id.recycler_names)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout,parent, false )
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.items_layout,parent, false )
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = dataList[position].name
+        val person = personList[position]
+        holder.textView.text = person.name
     }
 
     override fun getItemCount(): Int {
-        return dataList.size
+        return personList.size
     }
 
 }
