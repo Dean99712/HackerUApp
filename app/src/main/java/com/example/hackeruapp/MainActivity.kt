dbfeb689f99618f9eb0441506483962bf4761988
Dean99712 <Dean2910997@gmail.com>
2022-09-05 23:39:19 +0300

    implement Database into the Application Person class changed to Note

--- app/src/main/java/com/example/hackeruapp/MainActivity.kt
+++ app/src/main/java/com/example/hackeruapp/MainActivity.kt
@@ -3,23 +3,34 @@ package com.example.hackeruapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.EditText
 import androidx.core.os.bundleOf
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var repository: Repository
+    private val noteList = arrayListOf<Note>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setButtonClickListener()
     }
 
-    private fun displayPersonDetailsFragment(person: Person) {
+    override fun onStart() {
+        super.onStart()
+        setButtonClickListener()
+        val dao = NotesDatabase.getDatabase(application).getNotesDao()
+        repository = Repository(dao)
+
+        createRecyclerView()
+    }
+
+    private fun displayPersonDetailsFragment(note: Note) {
         val personFragment = PersonFragment()
-        val bundle = bundleOf("thePersonAge" to person.age, "thePersonName" to person.name)
+        val bundle = bundleOf("thePersonAge" to note.title, "thePersonName" to note.description)
         personFragment.arguments = bundle
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container_view, personFragment)
@@ -29,40 +40,24 @@ class MainActivity : AppCompatActivity() {
     private fun setButtonClickListener() {
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
-//            Toast.makeText(this, "Hi Im toast", Toast.LENGTH_LONG).show()
-            createRecyclerView()
-//            createViewProgrematic()
+            val noteTitle = findViewById<EditText>(R.id.note_title).text.toString()
+            val noteDesc = findViewById<EditText>(R.id.note_description).text.toString()
+            val note = Note(noteTitle, noteDesc)
+            thread(start = true) {
+                repository.addNote(note)
+            }
         }
     }
 
-    private fun createViewProgrematic() {
-        val secondTitleTextView = TextView(applicationContext)
-        secondTitleTextView.textSize = 30F;
-        secondTitleTextView.text = "Hi Im second title";
-        val constraintLayout = findViewById<ConstraintLayout>(R.id.screen)
-        constraintLayout.addView(secondTitleTextView)
-    }
-
-    private fun getPersonList(): MutableList<Person> {
-        val personList = mutableListOf<Person>()
-        personList.add(Person("Daniella", 30))
-        personList.add(Person("Boaz", 26))
-        personList.add(Person("Shoham", 28))
-        personList.add(Person("Ron", 31))
-        personList.add(Person("Naor", 43))
-        personList.add(Person("Mia", 29))
-        personList.add(Person("Oriel", 23))
-        personList.add(Person("Daniella", 50))
-        return personList
-    }
 
     private fun createRecyclerView() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        val personList = getPersonList()
-        val adapter = MyAdapter(personList) {
+        val adapter = MyAdapter(noteList) {
             displayPersonDetailsFragment(it)
         }
         recyclerView.adapter = adapter
+        repository.getAllNotes().observe(this) {
+            adapter.updateAdapterView(it)
+        }
     }
-
 }
---
+++ app/src/main/java/com/example/hackeruapp/Repository.kt
@@ -0,0 +1,14 @@
+package com.example.hackeruapp
+
+import androidx.lifecycle.LiveData
+
+class Repository(private val notesDao: NotesDao) {
+
+    fun getAllNotes(): LiveData<List<Note>> {
+        return notesDao.getAllNotes()
+    }
+
+    fun addNote(note: Note) {
+        notesDao.insertNote(note)
+    }
+}
