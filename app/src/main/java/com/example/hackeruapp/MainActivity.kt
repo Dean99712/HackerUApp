5f5af425479a446bb3c5e0fb19eb4d102754f769
Dean99712 <Dean2910997@gmail.com>
2022-09-07 18:43:47 +0300

    removed unused methods and classes

--- app/src/main/java/com/example/hackeruapp/MainActivity.kt
+++ app/src/main/java/com/example/hackeruapp/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.hackeruapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
-import androidx.core.os.bundleOf
-import androidx.core.text.isDigitsOnly
 import androidx.recyclerview.widget.RecyclerView
 
 
@@ -12,23 +10,11 @@ class MainActivity : AppCompatActivity() {
 
     private var personList = arrayListOf<Person>()
     private var adapter = RecyclerAdapter(personList)
-//    {
-//        displayPersonFragment(it)
-//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setButtonClickListener()
-    }
-
-    private fun displayPersonFragment(person: Person) {
-        val bundle = bundleOf("name" to person.name, "image" to person.image)
-        val personFragment = PersonFragment()
-        personFragment.arguments = bundle
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.person_details_fragment, personFragment)
-            .commit()
     }
 
     private fun setButtonClickListener() {
